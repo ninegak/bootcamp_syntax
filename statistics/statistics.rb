5a1a82f2dc863eebca7ee4ed00c1c0089c55422c
ninegak <Chakkaphan@bananacoding.com>
2025-04-16 17:07:05 +0700

    create all required function

--- statistics/statistics.rb
+++ statistics/statistics.rb
@@ -5,23 +5,35 @@ class Statistics
   end
 
   def find_max
+    @numbers.max()
   end
 
   def find_min
+    @numbers.min()
   end
 
   def average
+    @numbers.inject{ |sum, el| sum + el.to_f } / @numbers.size
   end
 
   def mode
+    freq = @numbers.tally
+
+    p freq.select {|k, v| v == freq.values.max}.keys
   end
 
   def even_numbers
+    @numbers.select do |el|
+      el % 2 == 0
+    end
   end
 
-  def count_vowels()
+  def count_vowels(texts)
+    @texts = texts
+    @texts.count('aeiou')
   end
 
-  def factorial()
+  def factorial(n)
+    (1..n).inject(:*)
   end
 end
